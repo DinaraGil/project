a4984d16bde1daf07e713c33490e9ee4fceffcc3
Ruslan <mr.krasava34@mail.ru>
2026-09-08 21:51:04 +0300

    Добавил логику генератора

--- internal/shortener/generator.go
+++ internal/shortener/generator.go
@@ -1,10 +1,26 @@
 package shortener
 
-const (
-	CodeLength = 10
-	alphabet   = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_"
+import (
+	"crypto/rand"
+	"math/big"
 )
 
-func Generate() (string error) {
-	
+const (
+	CodeLength = 10
+	// 63 символа длина
+	alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_"
+)
+
+func Generate() (string, error) {
+	shortURL := make([]byte, CodeLength)
+	maximum := big.NewInt(int64(CodeLength))
+	for i := 0; i < len(shortURL); i++ {
+		n, err := rand.Int(rand.Reader, maximum)
+		if err != nil {
+			return "", err
+		}
+		shortURL[i] = alphabet[n.Int64()]
+	}
+	result := string(shortURL)
+	return result, nil
 }
---
+++ internal/shortener/generator_test.go
@@ -0,0 +1,53 @@
+package shortener
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+)
+
+func TestGenerate_Length(t *testing.T) {
+	realResult, err := Generate()
+	if len(realResult) != CodeLength {
+		t.Errorf("Длина искомого %d не равна ожидаемоей длине %d", len(realResult), CodeLength)
+	}
+	if err != nil {
+		t.Errorf("Generate вернула ошибку %v", err)
+	}
+}
+func TestGenerate_Alphabet(t *testing.T) {
+	re := regexp.MustCompile(`^[A-Za-z0-9_]{10}$`)
+	result, err := Generate()
+	if err != nil {
+		t.Fatal(fmt.Sprintf("Generate вернула ошибку %v", err))
+	}
+	ok := re.MatchString(result)
+	if !ok {
+		t.Errorf("Алфавит для генерации не был соблюден!")
+	}
+}
+func TestGenerate_NoPanicManyTimes(t *testing.T) {
+	for i := 0; i < 10000; i++ {
+		_, err := Generate()
+		if err != nil {
+			t.Errorf("Generate вернула ошибку %v", err)
+		}
+	}
+}
+func TestGenerate_Uniqueness(t *testing.T) {
+	storage := make(map[string]bool)
+	countToGenerate := 100000
+	var temp string
+	var err error
+	for i := 0; i < countToGenerate; i++ {
+		temp, err = Generate()
+		if err != nil {
+			t.Fatal(fmt.Printf("Generate вернула ошибку %v", err))
+		}
+		storage[temp] = true
+	}
+	if len(storage) != countToGenerate {
+		t.Errorf("Кол-во сгенерированных данных %d не равна ожидаемому кол-ву  %d", len(storage), countToGenerate)
+
+	}
+}
